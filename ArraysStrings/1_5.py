cf1556dcf5fda47854791701080c24561d1e2b8c
JoeKabongo <jonathankabongo23@gmail.com>
2021-02-18 06:52:16 -0800

    ediy 1.5

--- ArraysStrings/1_5.py
+++ ArraysStrings/1_5.py
@@ -29,21 +29,3 @@ def oneAway(s1, s2):
             index2 += 1
 
     return True
-
-
-def oneAway2(s1, s2):
-    if abs(len(s1) - len(s2)) > 1:
-        return False
-
-    freq = {}
-    for char in s1:
-        freq[char] = freq.get(char, 0) + 1
-
-    mismatches = 0
-    for char in s2:
-        if freq.get(char, 0) == 0:
-            mismatches += 1
-        else:
-            freq[char] -= 1
-
-    return mismatches < 2
