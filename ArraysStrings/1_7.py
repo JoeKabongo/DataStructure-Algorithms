45838e9d045175bab6a5347331c8aa8c8a642214
JoeKabongo <jonathankabongo23@gmail.com>
2021-02-28 09:14:38 -0800

    1_7

---
+++ ArraysStrings/1_7.py
@@ -0,0 +1,9 @@
+def rotateMatrix(matrix):
+    N = len(matrix)
+    for i in range(N):
+        for j in range(i+1, N):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+    for i in range(N):
+        for j in range(N//2):
+            matrix[i][j], matrix[i][N-j-1] = matrix[i][N-j-1], matrix[i][j]
