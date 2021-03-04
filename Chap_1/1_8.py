e07e8e3b1fc5b70c161ce2915eb646aa5a0e3356
JoeKabongo <jonathankabongo23@gmail.com>
2021-03-03 21:33:40 -0800

    2.6

---
+++ Chap_1/1_8.py
@@ -0,0 +1,81 @@
+def zeroMatrixSpace(matrix):
+    N = len(matrix)
+    M = len(matrix[0])
+
+    zero_rows = [False] * N
+    zero_cols = [False] * M
+
+    for i in range(N):
+        for j in range(M):
+            if matrix[i][j] == 0:
+                zero_rows[i] = True
+                zero_cols[j] = True
+
+    for row in range(N):
+        if zero_rows[row]:
+            for col in range(M):
+                matrix[row][col] = 0
+
+    for col in range(M):
+        if zero_cols[col]:
+            for row in range(N):
+                matrix[row][col] = 0
+
+
+def zeroMatrixNoSpace(matrix):
+    N = len(matrix)
+    M = len(matrix[0])
+
+    is_first_row_zero, is_first_col_zero = is_first_zero(matrix, N, M)
+
+    for i in range(1, N):
+        for j in range(1, M):
+            if matrix[i][j] == 0:
+                matrix[0][j] = 0
+                matrix[i][0] = 0
+
+    # make row zero
+    for i in range(N):
+        if matrix[i][0] == 0:
+            make_row_zero(matrix, i, M)
+
+    print(matrix)
+
+    # make colum zero
+    for i in range(M):
+        if matrix[0][i] == 0:
+            make_col_zero(matrix, i, N)
+
+    if is_first_row_zero:
+        make_row_zero(matrix, 0, M)
+
+    if is_first_col_zero:
+        make_col_zero(matrix, 0, N)
+
+
+def make_row_zero(matrix, row, size):
+    for i in range(size):
+        matrix[row][i] = 0
+
+
+def make_col_zero(matrix, col, size):
+    for i in range(size):
+        matrix[i][col] = 0
+
+
+def is_first_zero(matrix, N, M):
+    output = [False, False]
+    for i in range(N):
+        if matrix[i][0] == 0:
+            output[1] = True
+            break
+
+    for j in range(M):
+        if matrix[0][j] == 0:
+            output[0] = True
+
+    return output
+
+
+x = [[1, 1, 1], [1, 0, 1], [1, 1, 1]]
+zeroMatrixNoSpace(x)
---
+++ Chap_2/2_6.py
@@ -0,0 +1,64 @@
+def isPalidrome(head):
+    """ Return either head is a palidrom or not"""
+    if head is None or head.next is None:
+        return True
+
+    slow = head
+    fast = head
+
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+
+    # for odd lenght of linkedlist
+    if fast:
+        slow = slow.next
+
+    # reverse the second half of the linkedlist
+    right_reverse = reverse(slow)
+    left = head
+
+    while right_reverse:
+        # not a palifrom
+        if right_reverse.val != left.val:
+            return False
+        right_reverse = right_reverse.next
+        left = left.next
+
+    return True
+
+
+def reverse(node):
+    prev = None
+    while node:
+        next_node = node.next
+        node.next = prev
+        prev = node
+        node = next_node
+
+    return prev
+
+
+def isPalindrome(head: ListNode) -> bool:
+    """This time using a stack"""
+    if head is None or head.next is None:
+        return True
+
+    slow = head
+    fast = head
+    stack = []
+
+    while fast and fast.next:
+        stack.append(slow.val)
+        slow = slow.next
+        fast = fast.next.next
+
+    if fast:
+        slow = slow.next
+
+    while stack:
+        if stack.pop() != slow.val:
+            return False
+        slow = slow.next
+
+    return True
