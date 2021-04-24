149dfe43560765e98d61a11115e427005c12ea83
JoeKabongo <jonathankabongo23@gmail.com>
2021-04-24 16:43:56 -0700

    3.5

--- Chap_2/2_8.py
+++ Chap_2/2_8.py
@@ -1,3 +1,27 @@
-def loop_detection(head):
-    """ return node at beginning of loop, if there is one"""
-    pass
+from node import Node
+
+
+def linkedlist_beginning(head) -> ListNode:
+    """Find start of loop in linkedlist """
+    slow = head
+    fast = head
+
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+
+        if fast == slow:
+            break
+
+    # there is no loop in the linkedlist
+    if fast is None or fast.next is None:
+        return None
+
+    slow = head
+
+    # both slow and fast will be k step away from the start of loops
+    while slow != fast:
+        fast = fast.next
+        slow = slow.next
+
+    return fast
